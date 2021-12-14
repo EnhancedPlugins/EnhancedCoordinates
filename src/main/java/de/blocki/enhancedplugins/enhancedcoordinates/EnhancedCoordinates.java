5a219b731aa8f7c7a1cb44feca5723a3fc3af53c
Luca Heß <65811753+blockiyt@users.noreply.github.com>
2021-12-14 10:08:27 +0100

    init

---
+++ src/main/java/de/blocki/enhancedplugins/enhancedcoordinates/EnhancedCoordinates.java
@@ -0,0 +1,17 @@
+package de.blocki.enhancedplugins.enhancedcoordinates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnhancedCoordinates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
